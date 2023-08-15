2ea75dcb643f7ffcf3618ffcef6e2b1d836a2ade
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2023-08-15 11:48:48 -0400

    skeleton ui of home page done

--- withlove/ContentView.swift
+++ withlove/ContentView.swift
@@ -9,15 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(red: 0.996, green: 0.976, blue: 0.92)
-                .ignoresSafeArea()
-            VStack {
-                Text("With Love")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0.907, green: 0.56, blue: 0.556))
-                    .font(.system(size: 50))
-                    .multilineTextAlignment(.center)
+        NavigationStack() {
+            ZStack {
+                Color(red: 0.996, green: 0.976, blue: 0.92)
+                    .ignoresSafeArea()
+                VStack {
+                    HStack() {
+                        Spacer()
+                        Image("lettericon")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.trailing, 10.0)
+                            .frame(width: 50.0)
+                    }
+                    Spacer()
+                        .frame(height: 150.0)
+                    Text("With Love")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.907, green: 0.56, blue: 0.556))
+                        .font(.system(size: 50))
+                        .multilineTextAlignment(.center)
+                    HStack() {
+                        NavigationLink(destination: ReadView()) {
+                            VStack() {
+                                Image("lettericon")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100.0)
+                                Text("Read")
+                            }
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        NavigationLink(destination: ReadView()) {
+                            VStack() {
+                                Image("lettericon")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100.0)
+                                Text("Write")
+                            }
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    }
+                    NavigationLink(destination: ReadView()) {
+                        VStack() {
+                            Image("lettericon")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100.0)
+                            Text("My letters")
+                        }
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                        .frame(height: 150)
+                }
             }
         }
     }
---
+++ withlove/ReadView.swift
@@ -0,0 +1,20 @@
+//
+//  ReadView.swift
+//  withlove
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+struct ReadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReadView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadView()
+    }
+}
